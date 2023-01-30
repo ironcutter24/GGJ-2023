47f3d6b58c99cfe526dd944e87cbb24556567686
Antonio Troiano <antonio.troiano2304@gmail.com>
2023-01-30 17:19:23 +0100

    Commit: Fix Jump // Stick action
    - Now the player Can prop jump
    - Now the player can Stick

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -62,6 +62,15 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Stick"",
+                    ""type"": ""Button"",
+                    ""id"": ""c0ed729d-f4d2-483f-86aa-996382e94651"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -293,6 +302,17 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
                     ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4db59745-d39e-458d-b15c-07711210505c"",
+                    ""path"": ""<Keyboard>/p"",
+                    ""interactions"": ""Hold"",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Stick"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -884,6 +904,7 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Stick = m_Player.FindAction("Stick", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -959,6 +980,7 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
     private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Stick;
     public struct PlayerActions
     {
         private @PlayerControls m_Wrapper;
@@ -967,6 +989,7 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
         public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Stick => m_Wrapper.m_Player_Stick;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -988,6 +1011,9 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
                 @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
                 @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Stick.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnStick;
+                @Stick.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnStick;
+                @Stick.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnStick;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -1004,6 +1030,9 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
                 @Jump.started += instance.OnJump;
                 @Jump.performed += instance.OnJump;
                 @Jump.canceled += instance.OnJump;
+                @Stick.started += instance.OnStick;
+                @Stick.performed += instance.OnStick;
+                @Stick.canceled += instance.OnStick;
             }
         }
     }
@@ -1164,6 +1193,7 @@ public partial class @PlayerControls : IInputActionCollection2, IDisposable
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnStick(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,38 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float movement_speed = 5f;
+    private float movement_speed, jump_power = 5f;
     [SerializeField]
-    private float jump_power = 5f;
+    [Tooltip("Distance of the raycast to the ground for the jump")]
+    private float buffer_check_distance = 0.3f;
+    [SerializeField]
+    [Tooltip("DO NOT TOUCH")]
+    private LayerMask layerMask;
 
     private PlayerControls player_controls;
     private InputAction player_move;
     private Vector2 move_direction;
     private Rigidbody2D player_rb;
+    private CapsuleCollider2D player_capsule;
+    private bool is_facing_right = true;
+    private bool is_sticking = false;
+    private RaycastHit2D hit;
+    private GameObject under_platform;
 
     private void Awake()
     {
         player_controls = new PlayerControls();
         player_rb = GetComponent<Rigidbody2D>();
+        player_capsule = GetComponent<CapsuleCollider2D>();
     }
 
     void Start()
@@ -39,25 +53,60 @@ public class Player : MonoBehaviour
     void Update()
     {
         player_rb.velocity = new Vector2(move_direction.x * movement_speed, player_rb.velocity.y);
+        //Debug.DrawRay(new Vector3(player_capsule.bounds.center.x, player_capsule.bounds.min.y, 0), new Vector3(0, -buffer_check_distance, 0), Color.blue);
+
+        if (!is_facing_right && move_direction.x > 0f || is_facing_right && move_direction.x < 0f)
+            Flip();
     }
 
     private void FixedUpdate()
     {
-        /*player_rb.AddForce(move_direction * movement_speed * Time.deltaTime);*/
+    }
+
+    private bool IsGrounded()
+    {
+        hit = Physics2D.Raycast(new Vector2(player_capsule.bounds.center.x, player_capsule.bounds.min.y), -transform.up, buffer_check_distance, layerMask);
+        return hit.collider != null;
+    }
+
+    private void Flip()
+    {
+        is_facing_right = !is_facing_right;
+        transform.Rotate(new Vector3(0, 180, 0));
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        move_direction = context.ReadValue<Vector2>();
-
+        if (!is_sticking)
+            move_direction = context.ReadValue<Vector2>();
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && IsGrounded() && !is_sticking)
             player_rb.velocity = new Vector2(player_rb.velocity.x, jump_power);
 
-        if (context.canceled && player_rb.velocity.y > 0f)
+        if (context.canceled && player_rb.velocity.y > 0f && !is_sticking)
             player_rb.velocity = new Vector2(player_rb.velocity.x, player_rb.velocity.y * 0.5f);
+    }
+
+    public void StickOnFloor(InputAction.CallbackContext context)
+    {
+        if (context.started && IsGrounded())
+        {
+            is_sticking = true;
+            player_rb.simulated = false;
+            print(hit.collider.transform.parent.name);
+            transform.SetParent(hit.collider.transform.parent.transform);
+            move_direction = Vector2.zero;
+        }
+
+        if (context.canceled)
+        {
+            is_sticking = false;
+            player_rb.simulated = true;
+            transform.parent = null;
+            transform.rotation = Quaternion.Euler(0, is_facing_right? 0 : 180 , 0);
+        }
     }
 }
