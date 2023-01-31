85169313c567dffcd3d535b7eccf1db1e7b15b35
ironcutter24 <ironcutter24@gmail.com>
2023-01-31 03:00:26 +0100

    Waves update

--- Assets/Scripts/Damiano/SoundWavesVFX.cs
+++ Assets/Scripts/Damiano/SoundWavesVFX.cs
@@ -9,29 +9,31 @@ public class SoundWavesVFX : MonoBehaviour
     GameObject wavePrefab;
 
     [SerializeField]
-    float minScale = 1, maxScale = 2;
+    float minScale = 1, period = 2f;
 
     List<SpriteRenderer> circles = new List<SpriteRenderer>();
 
     void Start()
     {
-        //StartCoroutine(_WaveAnimation());
+        //StartCoroutine(_WaveAnimation(5));
     }
 
     public void Play(float range)
     {
-        StartCoroutine(_WaveAnimation());
+        StartCoroutine(_WaveAnimation(range));
     }
 
-    IEnumerator _WaveAnimation()
+    IEnumerator _WaveAnimation(float range)
     {
+        float targetScale = range * .4f;
+
         for (int i = 0; i < 6; i++)
         {
             var c = Instantiate(wavePrefab, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>();
             c.color = new Color(1f, 1f, 1f, 1f);
             c.transform.localScale = Vector3.one * minScale;
-            c.transform.DOScale(maxScale, 2f).SetEase(Ease.OutCubic);
-            c.DOColor(new Color(1f, 1f, 1f, 0f), 1.6f).SetEase(Ease.OutQuart);
+            c.transform.DOScale(targetScale, period).SetEase(Ease.OutCubic);
+            c.DOColor(new Color(1f, 1f, 1f, 0f), period).SetEase(Ease.OutQuart);
 
             circles.Add(c);
 
