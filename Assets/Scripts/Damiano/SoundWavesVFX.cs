97cc5e6ffc58eed798e1a8ad540dae7796c788f8
ironcutter24 <ironcutter24@gmail.com>
2023-01-31 02:44:53 +0100

    Soundwave animation

--- Assets/Scripts/Damiano/SoundWavesVFX.cs
+++ Assets/Scripts/Damiano/SoundWavesVFX.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,34 @@ public class SoundWavesVFX : MonoBehaviour
     [SerializeField]
     GameObject wavePrefab;
 
-    void Play()
+    [SerializeField]
+    float minScale = 1, maxScale = 2;
+
+    List<SpriteRenderer> circles = new List<SpriteRenderer>();
+
+    void Start()
     {
-        var renderer = Instantiate(wavePrefab, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>();
-        renderer.color = new Color(1f, 1f, 1f, .4f);
+        //StartCoroutine(_WaveAnimation());
+    }
 
+    public void Play(float range)
+    {
+        StartCoroutine(_WaveAnimation());
+    }
 
+    IEnumerator _WaveAnimation()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            var c = Instantiate(wavePrefab, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>();
+            c.color = new Color(1f, 1f, 1f, 1f);
+            c.transform.localScale = Vector3.one * minScale;
+            c.transform.DOScale(maxScale, 2f).SetEase(Ease.OutCubic);
+            c.DOColor(new Color(1f, 1f, 1f, 0f), 1.6f).SetEase(Ease.OutQuart);
+
+            circles.Add(c);
+
+            yield return new WaitForSeconds(.2f);
+        }
     }
 }
