976c28d0a54269b8a447a240566db58f27c351ef
ironcutter24 <ironcutter24@gmail.com>
2023-02-01 16:12:21 +0100

    WorldTilemap script

---
+++ Assets/WorldTilemap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTilemap : MonoBehaviour
+{
+    void Awake()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Ground");
+
+        var rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+            rb = gameObject.AddComponent<Rigidbody2D>();
+
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        var composite = GetComponent<CompositeCollider2D>();
+        if (composite == null)
+            composite = gameObject.AddComponent<CompositeCollider2D>();
+    }
+}
